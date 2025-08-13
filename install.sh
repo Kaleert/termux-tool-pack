3e09b7f5149981cf7bfb02a525d4ed0fd87387df
Kaleert <142114945+Kaleert@users.noreply.github.com>
2025-08-13 12:12:05 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -156,7 +156,7 @@ install_base() {
         info_msg \"Installing required packages...\"
         for pkg in \"${packages[@]}\"; do
             info_msg \"Installing $pkg...\"
-            if apt-get install -qq -y --allow-downgrades --allow-remove-essential "$pkg" >/dev/null 2>&1; then
+            if apt-get install -qq -y --allow-downgrades --allow-remove-essential \"$pkg\" >/dev/null 2>&1; then
                 success_msg \"$pkg installed successfully\"
             else
                 error_msg \"Failed to install $pkg\"
